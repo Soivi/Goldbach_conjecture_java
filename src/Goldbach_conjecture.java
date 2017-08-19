c07619cfa3afc344e93ca496e5dbea4d16f3c010
Lauri Soivi <soivi.lauri@gmail.com>
2017-08-19 16:01:08 +0300

    loop with variables

--- src/Goldbach_conjecture.java
+++ src/Goldbach_conjecture.java
@@ -2,21 +2,19 @@
 public class Goldbach_conjecture {
 
 	public static void main(String[] args) {
-		for (int checkNum = 2; checkNum <= 10; checkNum++) {
-			boolean isPrime = checkPrime(checkNum);
-			if (isPrime)
+		int checkedNumber = 10;
+		for (int checkNum = 2; checkNum <= checkedNumber; checkNum++) {
+			boolean checkOtherPrime = checkPrime(checkNum);
+			if (checkOtherPrime) {
 				System.out.println(checkNum + " is Prime Number");
-			else
-				System.out.println(checkNum + " is not Prime Number");
-
-			// System.out.println(result);
+			}
 		}
+		// System.out.println(result);
 	}
-	
+
 	private static boolean checkPrime(int checkNum) {
 		boolean isPrime = true;
 		int remainderNum;
-
 		for (int i = 2; i <= checkNum / 2; i++) {
 			remainderNum = checkNum % i;
 			if (remainderNum == 0) {
@@ -24,7 +22,6 @@ public class Goldbach_conjecture {
 				break;
 			}
 		}
-		
 		return isPrime;
 	}
 }

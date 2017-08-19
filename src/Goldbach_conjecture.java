d0fe135c4744d7dbc92009f15c68d0738548b18d
Lauri Soivi <soivi.lauri@gmail.com>
2017-08-19 15:52:07 +0300

    checkPrime method added

--- src/Goldbach_conjecture.java
+++ src/Goldbach_conjecture.java
@@ -3,15 +3,7 @@ public class Goldbach_conjecture {
 
 	public static void main(String[] args) {
 		for (int checkNum = 2; checkNum <= 10; checkNum++) {
-			int remainderNum;
-			boolean isPrime = true;
-			for (int i = 2; i <= checkNum / 2; i++) {
-				remainderNum = checkNum % i;
-				if (remainderNum == 0) {
-					isPrime = false;
-					break;
-				}
-			}
+			boolean isPrime = checkPrime(checkNum);
 			if (isPrime)
 				System.out.println(checkNum + " is Prime Number");
 			else
@@ -19,5 +11,20 @@ public class Goldbach_conjecture {
 
 			// System.out.println(result);
 		}
+	}
+	
+	private static boolean checkPrime(int checkNum) {
+		boolean isPrime = true;
+		int remainderNum;
+
+		for (int i = 2; i <= checkNum / 2; i++) {
+			remainderNum = checkNum % i;
+			if (remainderNum == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		
+		return isPrime;
 	}
 }

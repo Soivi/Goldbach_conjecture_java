707aedea96e7c632898da0fc437583a2d19a95a8
Lauri Soivi <soivi.lauri@gmail.com>
2017-08-19 15:47:08 +0300

    loop numbers and check prime number

--- src/Goldbach_conjecture.java
+++ src/Goldbach_conjecture.java
@@ -2,9 +2,22 @@
 public class Goldbach_conjecture {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int checkNum = 2; checkNum <= 10; checkNum++) {
+			int remainderNum;
+			boolean isPrime = true;
+			for (int i = 2; i <= checkNum / 2; i++) {
+				remainderNum = checkNum % i;
+				if (remainderNum == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime)
+				System.out.println(checkNum + " is Prime Number");
+			else
+				System.out.println(checkNum + " is not Prime Number");
 
-		System.out.println("Hello World");
+			// System.out.println(result);
+		}
 	}
-
 }
